e428352505c5f3326b7efbdaa0970216c3baa193
ccssmania <ccss@utp.edu.co>
2019-01-29 23:21:38 +0100

    config database credentials

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'us-cdbr-iron-east-03.cleardb.net',
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => 'heroku_e870b638a3beb0c',
-            'username' => 'b84770e2367d54',
-            'password' => '6cf56365',
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
